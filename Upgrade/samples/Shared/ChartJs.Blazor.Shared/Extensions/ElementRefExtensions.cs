fd1fc1063cae403e0224346a14c807e685f23550
Marius <marius.muntean@live.com>
2019-08-25 20:48:58 +0200

    Even more stuff just happened

---
+++ Upgrade/samples/Shared/ChartJs.Blazor.Shared/Extensions/ElementRefExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace ChartJs.Blazor.Shared.Extensions
+{
+    public static class ElementRefExtensions
+    {
+        public static Task<string> GetValue(this ElementReference elementRef, IJSRuntime jsRuntime)
+        {
+            return jsRuntime.InvokeAsync<string>("SampleFunctions.GetElementValue", elementRef);
+        }
+    }
+}
