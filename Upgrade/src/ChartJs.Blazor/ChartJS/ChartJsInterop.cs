e926715c5c614adf484e19eda13d19d1e44a8a2b
Marius <marius.muntean@live.com>
2019-09-20 13:31:12 +0200

    Brought a few more charts into the fold

--- Upgrade/src/ChartJs.Blazor/ChartJS/ChartJsInterop.cs
+++ Upgrade/src/ChartJs.Blazor/ChartJS/ChartJsInterop.cs
@@ -3,13 +3,8 @@ using ChartJs.Blazor.ChartJS.Common.Legends.OnClickHandler;
 using ChartJs.Blazor.ChartJS.Common.Legends.OnHover;
 using Microsoft.JSInterop;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ChartJs.Blazor.ChartJS
@@ -20,11 +15,11 @@ namespace ChartJs.Blazor.ChartJS
         {
             try
             {
-                return jsRuntime.InvokeAsync<bool>("ChartJSInterop.SetupChart", StripNulls2(chartConfig));
-                //return jsRuntime.InvokeAsync<bool>("ChartJSInterop.SetupChart", chartConfig);
+                return jsRuntime.InvokeAsync<bool>("ChartJSInterop.SetupChart", StripNulls(chartConfig));
             }
-            catch (Exception exp)
+            catch (Exception ex)
             {
+                Console.WriteLine($"SetupChart failed with: {ex}");
             }
 
             return new ValueTask<bool>(false);
@@ -34,62 +29,22 @@ namespace ChartJs.Blazor.ChartJS
         {
             try
             {
-                return jsRuntime.InvokeAsync<bool>("ChartJSInterop.UpdateChart", StripNulls2(chartConfig));
-                //return jsRuntime.InvokeAsync<bool>("ChartJSInterop.UpdateChart", chartConfig);
+                return jsRuntime.InvokeAsync<bool>("ChartJSInterop.UpdateChart", StripNulls(chartConfig));
             }
-            catch (Exception exp)
+            catch (Exception ex)
             {
+                Console.WriteLine($"UpdateChart failed with: {ex}");
             }
 
             return new ValueTask<bool>(false);
         }
 
-        /// <summary>
-        /// Returns an object that is equivalent to the given parameter but without any null member AND it preserves DotNetInstanceClickHandler/DotNetInstanceHoverHandler members intact
-        ///
-        /// <para>Preserving DotNetInstanceClick/HoverHandler members is important because they contain DotNetObjectRefs to the instance whose method should be invoked on click/hover</para>
-        ///
-        /// <para>This whole method is hacky af but necessary. Stripping null members is only needed because the default config for the Line charts on the Blazor side is somehow messed up. If this were not the case no null member stripping were necessary and hence, the recovery of the DotNetObjectRef members would also not be needed. Nevertheless, The Show must go on!</para>
-        /// </summary>
-        /// <param name="chartConfig"></param>
-        /// <returns></returns>
         private static object StripNulls(ChartConfigBase chartConfig)
         {
-            // Serializing with the custom serializer settings remove null members
+            // Serializing with the custom serializer settings to remove null members and switch to camel case
             var cleanChartConfigStr = JsonConvert.SerializeObject(chartConfig, JsonSerializerSettings);
 
-            // Get back an ExpandoObject dynamic with the clean config - having an ExpandoObject allows us to add/replace members regardless of type
-            dynamic clearConfigExpando = JsonConvert.DeserializeObject(cleanChartConfigStr,
-                typeof(ExpandoObject),
-                new ExpandoObjectConverter(){});
-
-            // Restore any .net refs that need to be passed intact
-            var dynamicChartConfig = (dynamic)chartConfig;
-            if (dynamicChartConfig?.Options?.Legend?.OnClick != null
-                && dynamicChartConfig?.Options?.Legend?.OnClick is DotNetInstanceClickHandler)
-            {
-                clearConfigExpando.options = clearConfigExpando.options ?? new { };
-                clearConfigExpando.options.legend = clearConfigExpando.options.legend ?? new { };
-                clearConfigExpando.options.legend.onClick = dynamicChartConfig.Options.Legend.OnClick;
-            }
-
-            if (dynamicChartConfig?.Options?.Legend?.OnHover != null
-                && dynamicChartConfig?.Options?.Legend?.OnHover is DotNetInstanceHoverHandler)
-            {
-                clearConfigExpando.options = clearConfigExpando.options ?? new { };
-                clearConfigExpando.options.legend = clearConfigExpando.options.legend ?? new { };
-                clearConfigExpando.options.legend.onHover = dynamicChartConfig.Options.Legend.OnHover;
-            }
-
-            return new Dictionary<string, object>(clearConfigExpando);
-        }
-
-        private static object StripNulls2(ChartConfigBase chartConfig)
-        {
-            // Serializing with the custom serializer settings remove null members
-            var cleanChartConfigStr = JsonConvert.SerializeObject(chartConfig, JsonSerializerSettings);
-
-            // Get back an ExpandoObject dynamic with the clean config - having an ExpandoObject allows us to add/replace members regardless of type
+            // Get back an a dynamic object in order to enhance it with .Net instance handlers
             dynamic cleanChartConfig = System.Text.Json.JsonSerializer.Deserialize(cleanChartConfigStr, typeof(object));
 
             // Restore any .net refs that need to be passed intact
@@ -112,13 +67,6 @@ namespace ChartJs.Blazor.ChartJS
 
             return cleanChartConfig;
         }
-
-        public static JsonSerializerOptions JsonSerializerSettings2 => new JsonSerializerOptions()
-        {
-            IgnoreNullValues = true,
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            
-        };
 
         private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
         {
